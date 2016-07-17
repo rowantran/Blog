ffba5e85350ea2b5b45d70d92f9360bd894e23ff
Rowan Tran <rowanjames.tran@gmail.com>
2016-07-17 00:12:23 -0700

    Basic features, need sanitization on NewPostHandler and PostHandler

--- udacity-blog-1/main.py
+++ udacity-blog-1/main.py
@@ -18,6 +18,7 @@
 import hashlib
 import hmac
 import jinja2
+import logging
 import os
 import random
 import string
@@ -39,15 +40,27 @@ class User(db.Model):
     password = db.StringProperty(required = True)
     salt = db.StringProperty(required = True)
 
-# Define functions
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    date = db.DateTimeProperty(auto_now_add = True)
+    body = db.TextProperty(required = True)
+
+
+# Hash functions
 def hash_username(username):
     return hmac.new(secret, username, hashlib.sha256).hexdigest()
 
 def hash_password(password, salt):
     return hmac.new(salt, password, hashlib.sha256).hexdigest()
 
+# Cookie functions
 def write_cookie(response, name, value):
     cookie = "{}={}".format(name, value)
+    response.headers.add_header('Set-Cookie', cookie)
+
+def delete_cookie(response, name):
+    cookie = "{}=; expires=Thu, 01 Jan 1970 00:00:00 GMT".format(name)
     response.headers.add_header('Set-Cookie', cookie)
 
 def generate_cookie(username):
@@ -56,6 +69,15 @@ def generate_cookie(username):
 def verify_cookie(cookie):
     username = cookie.split("|")[0]
     return cookie == generate_cookie(username)
+
+# Authentication functions
+def authenticate_user(user, password):
+    user_hash = hash_password(str(password), str(user.salt))
+    return user_hash == user.password
+
+# Other functions
+def username_exists(username):
+    return User.gql("WHERE username=:username", username=username).get()
 
 # Define handlers
 class Handler(webapp2.RequestHandler):
@@ -71,15 +93,22 @@ class Handler(webapp2.RequestHandler):
 
 
 class MainHandler(Handler):
+    def render_main(self, username='', auth=False):
+        posts = Post.all()
+        posts.order('-date')
+
+        self.render('main.html', username=username, posts=posts, auth=auth)
+
     def get(self):
         user = self.request.cookies.get('User')
         if not user:
-            self.redirect('/register')
+            self.render_main()
         else:
             if not verify_cookie(user):
-                self.redirect('/register')
+                self.render_main()
             else:
-                self.write('Welcome, {}!'.format(user.split("|")[0]))
+                username = user.split("|")[0]
+                self.render_main(username, True)
 
 
 class RegisterHandler(Handler):
@@ -102,9 +131,11 @@ class RegisterHandler(Handler):
             errors.append("Passwords must match.")
         if not password:
             errors.append("Password is required.")
-        
+        if username_exists(username):
+            errors.append("Username already exists.")
+    
         if errors:
-            self.render_register(username, email, "<br>".join(errors))
+            self.render_register(username, email, '<br>'.join(errors))
         else:
             salt = "".join(random.choice(string.letters) for i in xrange(16))
             password_hash = hash_password(password, salt)
@@ -116,7 +147,64 @@ class RegisterHandler(Handler):
             write_cookie(self.response, 'User', generate_cookie(username))
             self.redirect("/")
 
+
+class LoginHandler(Handler):
+    def render_login(self, username='', error=False):
+        if error:
+            error_message = "Username or password is incorrect."
+        else:
+            error_message = ""
+        self.render('login.html', username=username, error=error_message)
+
+    def get(self):
+        self.render_login()
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        user = User.gql('WHERE username=:username', username=username).get() 
+        if user:
+            if authenticate_user(user, password):
+                write_cookie(self.response, 'User', generate_cookie(username))
+                self.redirect("/")
+            else:
+                self.render_login(username, True)
+        else:
+            self.render_login(username, True)
+
+
+class LogoutHandler(Handler):
+    def get(self):
+        delete_cookie(self.response, 'User')
+        self.redirect("/")
+
+
+class NewPostHandler(Handler):
+    def get(self):
+        self.render('newpost.html')
+    
+    def post(self):
+        title = self.request.get('title')
+        body = self.request.get('body')
+
+        new_post = Post(title = title, body = body)
+        new_post.put()
+
+        self.redirect('/post/{post_id}'.format(post_id = new_post.key().id()))
+
+
+class PostHandler(Handler):
+    def get(self, post_id):
+        post = Post.get_by_id(int(post_id))
+        self.render('post.html', post=[post])
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/register', RegisterHandler)
+    webapp2.Route(r'/', handler=MainHandler, name='main'),
+    webapp2.Route(r'/register', handler=RegisterHandler, name='register'),
+    webapp2.Route(r'/login', handler=LoginHandler, name='login'),
+    webapp2.Route(r'/logout', handler=LogoutHandler, name='logout'),
+    webapp2.Route(r'/newpost', handler=NewPostHandler, name='newpost'),
+    webapp2.Route(r'/post/<post_id:\d+>', handler=PostHandler, name='post')
 ], debug=True)
